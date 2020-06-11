d5de73a80e7bc4e6add0ff4011de665e483a409d
Manuelpv17 <manuelpv17@outlook.com>
2020-06-11 13:30:55 -0500

    Add task 1 insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *f = NULL;
+	listint_t *b = NULL;
+	listint_t *aux = NULL;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	for (f = *list; f != NULL; f = f->next)
+	{
+		for (b = f; b->prev != NULL;)
+		{
+			if (b->prev->n > b->n)
+			{
+				if (b->prev->prev != NULL)
+					b->prev->prev->next = b;
+				else
+					*list = b;
+
+				if (b->next != NULL)
+					b->next->prev = b->prev;
+
+				aux = b->prev->prev;
+
+				b->prev->next = b->next;
+				b->prev->prev = b;
+
+				b->next = b->prev;
+				b->prev = aux;
+
+				print_list(*list);
+			}
+			else
+				break;
+		}
+	}
+	//printf("value: %d\n", f->n);
+}
