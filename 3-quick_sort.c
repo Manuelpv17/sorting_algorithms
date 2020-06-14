6f851ebcbf251c573bc912e6961a4c8476b95a12
Manuelpv17 <manuelpv17@outlook.com>
2020-06-14 16:23:35 -0500

    Rewrite code

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,6 @@
  */
 void quick_sort(int *array, size_t size)
 {
-
 	if (size > 1)
 		quick_aux(array, size, 0, size - 1);
 }
@@ -24,33 +23,33 @@ void quick_sort(int *array, size_t size)
  */
 void quick_aux(int *array, size_t size, size_t low, size_t high)
 {
-	size_t j, i = low;
+	size_t j;
+	long int i = low - 1;
 	size_t aux;
 
-	for (j = low; j < high + 1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < array[high])
 		{
-			if (array[i] != array[j])
+			i++;
+			if ((long int)j != i)
 			{
 				aux = array[j];
 				array[j] = array[i];
 				array[i] = aux;
 				print_array(array, size);
 			}
-			i++;
 		}
 	}
-	if (i != high)
-	{
-		aux = array[high];
-		array[high] = array[i];
-		array[i] = aux;
-		print_array(array, size);
-	}
+	i++;
+	aux = array[i];
+	array[i] = array[high];
+	array[high] = aux;
+	print_array(array, size);
 
-	if (high > i + 1)
+	if ((long int)high > i + 1)
 		quick_aux(array, size, i + 1, high);
-	if (i > low + 1)
+
+	if ((long int)low < i - 1)
 		quick_aux(array, size, low, i - 1);
 }
