7682a1be65115c01c0d859bd744a78497ef66fde
Manuelpv17 <manuelpv17@outlook.com>
2020-06-14 23:42:17 -0500

    Add task 8 heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+void heapify(int *array, size_t size, long int i, long int s);
+
+/**
+ * heap_sort - sorts an array of integers in ascending order 
+ * using the sift-down Heap sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of Array
+ */
+void heap_sort(int *array, size_t size)
+{
+	long int i, aux;
+
+	for (i = (size / 2) - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+
+	for (i = size - 1; i > 1; i--)
+	{
+		aux = array[0];
+		array[0] = array[i];
+		array[i] = aux;
+		print_array(array, size);
+		heapify(array, size, 0, i);
+	}
+}
+
+void heapify(int *array, size_t size, long int i, long int s)
+{
+	long int j, max = i, aux;
+
+	for (j = 2; j > 0 && s > (2 * i) + j; j--)
+		if (array[i] < array[(2 * i) + j] && array[max] < array[(2 * i) + j])
+			max = (2 * i) + j;
+
+	if (max != i)
+	{
+		aux = array[i];
+		array[i] = array[max];
+		array[max] = aux;
+		print_array(array, size);
+		heapify(array, size, max, s);
+	}
+}
